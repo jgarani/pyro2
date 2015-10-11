fed8dff0f92e22b2167f5909c74a95351b6fd33f
Jasmine Garani <Jasmine@Jasmines-MacBook-Air.local>
2015-10-11 11:39:34 -0400

    First commit

---
+++ compressible/problems/vortex.py
@@ -0,0 +1,103 @@
+from __future__ import print_function
+
+import math
+import numpy as np
+
+import sys
+import mesh.patch as patch
+from util import msg
+
+def init_data(my_data, rp):
+    """ initialize the vortex problem """
+
+    msg.bold("initializing the vortex problem...")
+    
+    
+    
+    
+    # make sure that we are passed a valid patch object
+    if not isinstance(my_data, patch.CellCenterData2d):
+        print("ERROR: patch invalid in vortex.py")
+        print(my_data.__class__)
+        sys.exit()
+
+    # get the density, momenta, and energy as separate variables
+    dens = my_data.get_var("density")
+    xmom = my_data.get_var("x-momentum")
+    ymom = my_data.get_var("y-momentum")
+    ener = my_data.get_var("energy")
+
+    gamma = rp.get_param("eos.gamma")
+
+    p0 = rp.get_param("vortex.p0")
+    t_r = rp.get_param("vortex.t_r")
+    mach = rp.get_param("vortex.mach")
+
+
+    # initialize the components, remember, that ener here is
+    # rho*eint + 0.5*rho*v**2, where eint is the specific
+    # internal energy (erg/g)
+    xmom.d[:,:] = 0.0
+    ymom.d[:,:] = 0.0
+    dens.d[:,:] = 0.0
+
+    # set the density to be stratified in the y-direction
+    myg = my_data.grid
+
+    x_c = 0.5*(myg.xmin + myg.xmax)
+    y_c = 0.5*(myg.ymin + myg.ymax)
+
+    p = myg.scratch_array()
+
+    dens.d[:,:] = 1.0
+
+    j = myg.jlo
+    while j <= myg.jhi:
+        i = myg.ilo
+        while i <= myg.ihi:
+
+            #initialize vortex problem
+            r=np.sqrt((myg.x[i]-x_c)**2 + (myg.y[j]-y_c)**2)
+           
+           
+            # phi_hat = -np.sin(phi)*x_hat+cos(phi)*y_hat    
+   
+            q_r=(0.4*np.pi)/t_r       
+    
+            if r< 0.2:
+                u_phi = 5*r
+            elif r < 0.4:
+                u_phi = 2.0-5.0*r
+            else:
+                u_phi = 0
+
+            u = -q_r*u_phi*((myg.y[j]-y_c)/r)
+            v = q_r*u_phi*((myg.x[i]-x_c)/r)
+
+            xmom.d[i,j] = dens.d[i,j]*u
+            ymom.d[i,j] = dens.d[i,j]*v
+            
+            p_0 = (dens.d[i,j]/(gamma*(mach)**2)-(1.0/2.0)) #u_phimax is 1
+  
+            if r<0.2:
+                p_r = p_0+(25.0/2)*(r**2)
+            elif r < 0.4:
+                p_r = p_0 + (25.0/2)*(r**2) + 4*(1.0-5.0*r-math.log(0.2)+math.log(r))
+            else:
+                p_r = p_0 - 2.0 + 4.0*math.log(2.0)
+            
+
+            p.d[i,j] = p_r
+
+            i += 1
+        j += 1
+
+
+    # set the energy (P = cs2*dens)
+    ener.d[:,:] = p.d[:,:]/(gamma - 1.0) + \
+        0.5*(xmom.d[:,:]**2 + ymom.d[:,:]**2)/dens.d[:,:]
+
+
+def finalize():
+    """ print out any information to the user at the end of the run """
+    pass
